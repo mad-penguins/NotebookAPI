99e1b37166bc9daa0330ee550cf565b80cd9798e
Terminator <fresh-ter@yandex.com>
2021-12-11 14:30:17 +0400

    Implement creating a new note

--- notebook.py
+++ notebook.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import json
 
 from utils import NotesDB
@@ -39,5 +39,23 @@ def get_note(note_id):
         response["note"] = note.dumpToDict()
     else:
         response["note"] = None
+
+    return json.dumps(response)
+
+
+@app.route('/notes/new', methods=('GET', 'POST'))
+def create_note():
+    notesDB = get_notesdb()
+
+    response = {}
+
+    print(request.values)
+    title = request.values.get('title', "some name")
+    text = request.values.get('text', "some text")
+    note = notesDB.createAndAddNewNote(title, text)
+
+    response["status"] = 'OK'
+    response["note_id"] = note.getID()
+
 
     return json.dumps(response)
